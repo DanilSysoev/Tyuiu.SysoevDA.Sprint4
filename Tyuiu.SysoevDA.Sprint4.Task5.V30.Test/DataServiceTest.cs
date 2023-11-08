b45baf2446b911c621252671bd19560d88c007a9
SysoevDA <fuzzii2739@gmail.com>
2023-11-08 11:49:23 +0500

    Task5

---
+++ Tyuiu.SysoevDA.Sprint4.Task5.V30.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SysoevDA.Sprint4.Task5.V30.Lib;
+namespace Tyuiu.SysoevDA.Sprint4.Task5.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = { { -2, 1, 3, 4, -1 },
+                             { -2, -1, 0, -2, 3 },
+                             { 1, 2, -2, 0, -1 },
+                             { 2, -1, 4, -2, 2 },
+                             { -2, -1, -2, 0, 1 } };
+            Assert.AreEqual(23, ds.Calculate(matrix));
+        }
+    }
+}
---
+++ Tyuiu.SysoevDA.Sprint4.Task5.V30/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SysoevDA.Sprint4.Task5.V30.Lib;
+
+namespace Tyuiu.SysoevDA.Sprint4.Task5.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Сысоев Д.А | ПКТБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Сысоев Данил Алексеевич | ПКТБ-23-1                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -2 до 4. Найти сумму положительных *");
+            Console.WriteLine("* элементов.                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцев в массиве: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[rows, columns];
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = rnd.Next(-2, 4);
+                }
+            }
+
+            Console.Write($"Массив: \n {"{"} ");
+            Console.WriteLine();
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("\t { ");
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j == 0)
+                    {
+                        Console.Write($" {array[i, j]}");
+                    }
+                    else
+                    {
+                        Console.Write($", {array[i, j]}");
+                    }
+                }
+                if (i != rows - 1)
+                {
+                    Console.Write(" },");
+                }
+                else
+                {
+                    Console.Write(" } ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" }");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма положительных элементов массива = " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
